ff3159f7cb5d8c94d8919c183324c3aabbf5ead4
shiori.kato <shakespeare14.amika@gmail.com>
2021-04-12 13:02:27 +0000

    tasklist post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,23 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  before_action :set_message, only: [:show, :edit, :update]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(5)
+    if logged_in?
+    @tasks = current_user.tasks.page(params[:page]).per(5)
+    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが正常に作成されました"
@@ -39,9 +43,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = "タスクは正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
 private
@@ -56,4 +59,10 @@ private
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
