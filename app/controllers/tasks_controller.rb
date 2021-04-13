28e98ea1772fcf479e763d1f2f1f50a9a3b57c43
shiori.kato <shakespeare14.amika@gmail.com>
2021-04-13 12:34:29 +0000

    tasklist re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:destroy]
+  before_action :set_task, only: [:show, :edit]
+  before_action :correct_user, only: [:update, :destroy]
   
   def index
     @tasks = current_user.tasks.page(params[:page]).per(5)
@@ -30,7 +30,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    
     if @task.update(task_params)
       flash[:success] = "タスクは正常に更新されました"
       redirect_to @task
@@ -43,13 +42,10 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = "タスクは正常に削除されました"
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 
-  
   private
-
-  
 
 # Strong Parameter
 
@@ -60,7 +56,7 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
-  
+
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
- 
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "tasks#index"
+ root to: "tasks#index"
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
